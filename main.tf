39b7409452aa8b3b03d0ce880f11bdcc7e376dec
Lilihc3006 <lilitacorazon111@gmail.com>
2025-07-19 22:36:23 -0500

    fix: remediate vulnerabilities detected by tfsec

--- main.tf
+++ main.tf
@@ -1,16 +1,21 @@
-resource "aws_security_group" "bad_example" {
-  name        = "insecure_sg"
-  description = "Allows all traffic"
-  vpc_id      = "vpc-123456"
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_security_group" "secure_example" {
+  name        = "allow_internal_http"
+  description = "Security group for internal HTTP access"
 
   ingress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    description = "Allow HTTP from internal network"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["192.168.1.0/24"]
   }
 
   egress {
+    description = "Allow all outbound traffic"
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
