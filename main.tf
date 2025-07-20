14d3a85ced714a22a94156249c5b9bbefc242bb3
Lilihc3006 <lilitacorazon111@gmail.com>
2025-07-20 10:30:44 -0500

    add insecure security group for tfsec validation

--- main.tf
+++ main.tf
@@ -24,3 +24,22 @@ resource "aws_security_group" "secure_example" {
     cidr_blocks = ["10.0.0.0/16"]
   }
 }
+resource "aws_security_group" "insecure_sg" {
+  name        = "allow_all"
+  description = "Allow all inbound traffic - Insecure"
+  vpc_id      = "vpc-12345678"
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
