d54bc394b05f839b2799fbefb09730af4438853a
Lilihc3006 <lilitacorazon111@gmail.com>
2025-07-19 22:21:01 -0500

    Agrega regla insegura para validar tfsec

--- main.tf
+++ main.tf
@@ -1,16 +1,15 @@
-provider "aws" {
-  region = "us-east-1"
-}
-
 resource "aws_security_group" "bad_example" {
-  name        = "allow_all"
-  description = "Test inbound traffic - tfsec validation"
+  name        = "insecure_sg"
+  description = "Allows all traffic"
+  vpc_id      = "vpc-123456"
+
   ingress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
   egress {
     from_port   = 0
     to_port     = 0
